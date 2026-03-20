a58d847810fcbdcf1b1dc32e870d59b7d987b504
ShauryaBisht02 <shauryab212@gmail.com>
2026-03-20 22:47:05 +0530

    fix API Key for streamlit cloud

--- api.py
+++ api.py
@@ -1,9 +1,13 @@
 import requests
 import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
-API_KEY = os.getenv("API_KEY")
+try:
+    API_KEY = st.secrets["API_KEY"]
+except:
+    API_KEY = os.getenv("API_KEY")
 BASE_URL = "https://api.openweathermap.org/data/2.5"
 
 def get_current_weather(city):
